f7441f577502e673f32a15f009b6cc6b98a270e7
pomiboy <embiidjoel76@gmail.com>
2024-07-02 16:44:48 +0900

    react-hook-form practice fin.

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -31,17 +31,83 @@ import { useForm } from "react-hook-form";
 //   );
 // }
 
+interface FormData {
+  email: string;
+  username: string;
+  password1: string;
+  password2: string;
+}
+
 function TodoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<FormData>();
+  const onValid = (data: FormData) => {
+    if (data.password1 !== data.password2) {
+      setError(
+        "password2",
+        { message: "Password not same!" },
+        { shouldFocus: true }
+      );
+    }
+  };
+  const onInValid = (data: any) => {
+    console.log("something's wrong");
+  };
+  console.log(errors);
 
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="email" />
-        <input {...register("firstName")} placeholder="First Name" />
-        <input {...register("lastName")} placeholder="Last Name" />
-        <button>Add</button>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          width: "300px",
+          margin: "50px auto",
+        }}
+        onSubmit={handleSubmit(onValid, onInValid)}
+      >
+        <input
+          {...register("email", {
+            required: "Email address is required!",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/, // 정규표현식
+              message: "Only NAVER emails allowed!",
+            },
+          })}
+          placeholder="email"
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("username", {
+            required: "Username is required!",
+            minLength: { value: 10, message: "Should be over 10 letters!" },
+            validate: {
+              requireDaniel: (value) =>
+                !value.includes("daniel") ? "Should include 'daniel'" : true,
+              noNick: (value) =>
+                value.includes("nick") ? "No 'Nick' allowed" : true,
+            },
+          })}
+          placeholder="Username"
+        />
+        <span>{errors?.username?.message}</span>
+        <input
+          {...register("password1", { required: "Password is required!" })}
+          placeholder="Password"
+        />
+        <span>{errors?.password1?.message}</span>
+        <input
+          {...register("password2", { required: "Password is required!" })}
+          placeholder="Retype password"
+        />
+        <span>{errors?.password2?.message}</span>
+        <button style={{ backgroundColor: "skyblue" }}>Add</button>
       </form>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -9,12 +9,10 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <RecoilRoot>
       <ThemeProvider theme={darkTheme}>
         <App />
       </ThemeProvider>
     </RecoilRoot>
-  </React.StrictMode>
 );
 
