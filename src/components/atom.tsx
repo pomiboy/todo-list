c816921fc9cbe432f0d6595371def6f8a32167d7
pomiboy <embiidjoel76@gmail.com>
2024-07-03 05:21:47 +0900

    recoil-persist 관련 설명주석 추가

--- src/components/atom.tsx
+++ src/components/atom.tsx
@@ -1,12 +1,14 @@
 import { atom, selector } from "recoil";
 import { recoilPersist } from "recoil-persist";
 
+// todo 저장할 localStorage 생성
 const { persistAtom: todoStorage } = recoilPersist({
   key: "todoStorage",
   storage: localStorage,
   converter: JSON,
 });
 
+// 현재 category 저장할 localStorage 생성 -> 새로고침해도 TO_DO로 바뀌지 않고 category 유지
 const { persistAtom: categoryStorage } = recoilPersist({
   key: "categoryStorage",
   storage: localStorage,
@@ -27,13 +29,13 @@ export interface ITodo {
 export const todoState = atom<ITodo[]>({
   key: "toDo",
   default: [],
-  effects_UNSTABLE: [todoStorage],
+  effects_UNSTABLE: [todoStorage], // todoState atom 값을 todoStorage에 저장하겠다
 });
 
 export const categoryState = atom<Categories>({
   key: "category",
   default: Categories.TO_DO,
-  effects_UNSTABLE: [categoryStorage],
+  effects_UNSTABLE: [categoryStorage], // categoryState atom 값을 categoryStorage 저장하겠다
 });
 
 export const todoSelector = selector({
