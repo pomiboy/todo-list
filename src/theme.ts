66c43480af4a5baaecf77f153b33fdb39eabda84
pomiboy <embiidjoel76@gmail.com>
2024-07-02 14:01:08 +0900

    create-react-app

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  textColor: "#d2dae2",
+  bgColor: "#2d3436",
+  accentColor: "#0be881",
+  coinTextColor: "black",
+};
